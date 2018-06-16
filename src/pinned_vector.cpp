42f440e1e5fa98ffdf3dcac04e4bf2c527bbfc80
Miro Knejp <miro.knejp@gmail.com>
2018-06-16 18:19:45 +0200

    Fix free() call on win32
    When called with MEM_RELEASE the number of bytes must be 0

--- src/pinned_vector.cpp
+++ src/pinned_vector.cpp
@@ -44,7 +44,7 @@ auto mknejp::detail::_pinned_vector::virtual_memory_allocator::reserve(std::size
 auto mknejp::detail::_pinned_vector::virtual_memory_allocator::free(void* offset, std::size_t num_bytes) -> void
 {
 #ifdef WIN32
-  assert(::VirtualFree(offset, num_bytes, MEM_RELEASE) != 0);
+  assert(::VirtualFree(offset, 0, MEM_RELEASE) != 0);
 #else
   assert(::munmap(offset, num_bytes) == 0);
 #endif
