51af8803203170030d3674f600a26eb2d2627a09
Miro Knejp <miro.knejp@gmail.com>
2018-09-25 04:32:39 +0200

    Rename pinned_vector.cpp to vm.cpp

--- src/vm.cpp
+++ src/vm.cpp
@@ -4,7 +4,7 @@
 // (See accompanying file LICENSE.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include "vmcontainer/pinned_vector.hpp"
+#include "vmcontainer/vm.hpp"
 
 #ifdef WIN32
 #  define NOMINMAX
