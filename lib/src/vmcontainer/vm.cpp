450e294bc35044a5bde8846c2c51cd5e5a663e1f
Norbert Wenzel <norbertwenzel@users.noreply.github.com>
2022-01-26 15:19:16 +0100

    Guard against Win32 macro redefinition (#7)

--- lib/src/vmcontainer/vm.cpp
+++ lib/src/vmcontainer/vm.cpp
@@ -7,8 +7,12 @@
 #include "vmcontainer/vm.hpp"
 
 #ifdef WIN32
-#  define NOMINMAX
-#  define WIN32_LEAN_AND_MEAN
+#  ifndef NOMINMAX
+#    define NOMINMAX
+#  endif
+#  ifndef WIN32_LEAN_AND_MEAN
+#    define WIN32_LEAN_AND_MEAN
+#  endif
 #  include <windows.h>
 #else
 #  include <sys/mman.h>
